512d0a0ed9b574a142a7b0e3a03ae3f80349c9f3
Jose Rocha <josemsrocha1044@gmail.com>
2022-12-22 21:55:18 -0300

    configs planetScale

---
+++ server/prisma/migrations/20221223002432_create_games/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE `Game` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `bannerUrl` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Ad` (
+    `id` VARCHAR(191) NOT NULL,
+    `gameId` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `yearsPlaying` INTEGER NOT NULL,
+    `discord` VARCHAR(191) NOT NULL,
+    `weekDays` VARCHAR(191) NOT NULL,
+    `hoursStart` INTEGER NOT NULL,
+    `hoursEnd` INTEGER NOT NULL,
+    `useVoiceChannel` BOOLEAN NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
